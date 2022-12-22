978e24025cea60fb54d7739452e3e8b785ecf2c6
JJ <JJ@JJ>
2022-12-22 11:08:14 +0900

    20221222 jjkim first

---
+++ shop/src/main/java/com/shop/dto/shopDTO.java
@@ -0,0 +1,16 @@
+package com.shop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class shopDTO {
+	private String id;
+	private String pwd;
+	private String name;
+}
