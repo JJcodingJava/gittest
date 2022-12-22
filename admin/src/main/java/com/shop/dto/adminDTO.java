8bb8e96eca0dc6443e7e42a515849e51f13861ac
JJ <JJ@JJ>
2022-12-22 11:23:43 +0900

    20221222 jjkim

---
+++ admin/src/main/java/com/shop/dto/adminDTO.java
@@ -0,0 +1,16 @@
+package com.shop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class adminDTO {
+	private String id;
+	private String pwd;
+	private String name;
+}
